c05cd61702648451be79164c999f1174a3adbf54
p3rsidh <persidh.pimenta@gmail.com>
2022-06-28 12:55:58 -0300

    Criando switch para escolher o dia correspondente, avisando se o numero é invalido,

--- src/lista03A/exercicio05/DiaDaSemana.java
+++ src/lista03A/exercicio05/DiaDaSemana.java
@@ -1,8 +1,45 @@
 package lista03A.exercicio05;
 
+import java.util.Scanner;
+
 public class DiaDaSemana {
     public static void main(String[] args) {
-        System.out.println("Aqui você pode introduzir um número e receber o dia correspondente da semana");
+        int numero;
+        Scanner input = new Scanner(System.in);
+        boolean validade=false;
 
+        System.out.println("Aqui você pode introduzir um número e receber o dia correspondente da semana");
+        while (validade == false){
+            System.out.println("Introduza um valor entre 1 e 7");
+            numero = input.nextInt();
+
+            if(numero > 1 && numero < 7){
+                validade = true;
+            switch (numero) {
+                case 1:
+                    System.out.println("DOMINGO");
+                    break;
+                case 2:
+                    System.out.println("SEGUNDA");
+                    break;
+                case 3:
+                    System.out.println("TERÇA");
+                    break;
+                case 4:
+                    System.out.println("QUARTA");
+                    break;
+                case 5:
+                    System.out.println("QUINTA");
+                    break;
+                case 6:
+                    System.out.println("SEXTA");
+                    break;
+                case 7:
+                    System.out.println("SABADO");
+                    break;
+
+            }}else
+                System.out.println("Numero invalido");
+        }
     }
 }
