50084f022359d74489be4b60c0228d0641aa076b
p3rsidh <persidh.pimenta@gmail.com>
2022-06-28 10:45:17 -0300

    Criando algorítimo que informa o objetivo ao usuário e recebe dois valores.

--- src/lista03A/exercicio01/ImprimirMaior.java
+++ src/lista03A/exercicio01/ImprimirMaior.java
@@ -1,7 +1,20 @@
 package lista03A.exercicio01;
 
+import java.awt.font.LineMetrics;
+import java.util.Scanner;
+
 public class ImprimirMaior {
     public static void main(String[] args) {
+        float valor1, valor2, maior;
+        Scanner input = new Scanner(System.in);
 
+        System.out.println("Aqui você pode introduzir dois números, e sera informado o maior");
+        System.out.println("Insira o primeiro número");
+        valor1 = input.nextFloat();
+        System.out.println("Insira o segundo número");
+        valor2 = input.nextFloat();
+        System.out.println("Processando informações");
+
+        }
     }
 }
