4f3559c8d2c6902d9e34d31907d0ececed8e61ff
p3rsidh <persidh.pimenta@gmail.com>
2022-06-28 13:02:37 -0300

    Contando usando for, depois usando while.

--- src/lista03B/exercicio1/Contador.java
+++ src/lista03B/exercicio1/Contador.java
@@ -6,5 +6,13 @@ public class Contador {
         for (int i = 1; i<101; i++){
             System.out.println(i);
         }
+
+        System.out.println("Vamos contar mais uma vez");
+        int i=1;
+        while (i < 101){
+            System.out.println(i);
+            i++;
+
+        }
     }
 }
