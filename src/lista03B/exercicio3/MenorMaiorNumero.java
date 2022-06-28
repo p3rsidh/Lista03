c20776c6f6f7e3cc5ef316d33af5a443d5f3c33c
p3rsidh <persidh.pimenta@gmail.com>
2022-06-28 20:46:10 -0300

    Separando as funçoes add e compare, retirando o print da repetição.

--- src/lista03B/exercicio3/MenorMaiorNumero.java
+++ src/lista03B/exercicio3/MenorMaiorNumero.java
@@ -6,21 +6,27 @@ import java.util.Scanner;
 
 public class MenorMaiorNumero {
     public static void main(String[] args) {
-        int menorNumero = 0, maiorNumero = 0;
+        int menorNumero , maiorNumero ;
         List<Integer> lista = new ArrayList<>();
         Scanner input = new Scanner(System.in);
         System.out.println("Introduza 10 numeros para saber o menor");
-
-        for(int i=0; i<10; i++) {
-            System.out.println("Digite o primeiro numero");
+        System.out.println("Digite o primeiro numero");
+        lista.add(input.nextInt());
+        menorNumero = lista.get(0);
+        maiorNumero = lista.get(0);
+        for (int i = 0; i<9; i++){
+            System.out.println("Digite mais um numero");
             lista.add(input.nextInt());
+        }
+        for(int i=0; i<10; i++) {
             if(lista.get(i) < menorNumero){
                 menorNumero = lista.get(i);
             }
-            if (lista.get(i) > maiorNumero){
+            if (lista.get(i) > maiorNumero) {
                 maiorNumero = lista.get(i);
+            }
         }
-            System.out.println("O maior numero e: " +maiorNumero);
-            System.out.println("O menor numero e :" +menorNumero);
-        }
+
+        System.out.println("O maior numero e: " +maiorNumero);
+        System.out.println("O menor numero e :" +menorNumero);
 }}
