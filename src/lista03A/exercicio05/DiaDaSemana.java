ea43d7913ed948fef3276c1420a3261539bfd2d0
p3rsidh <persidh.pimenta@gmail.com>
2022-06-28 12:40:44 -0300

    Criando nova classe para receber algoritimo que recebe do usuario o numero e exibe o dia da semana.

---
+++ src/lista03A/exercicio05/DiaDaSemana.java
@@ -0,0 +1,8 @@
+package lista03A.exercicio05;
+
+public class DiaDaSemana {
+    public static void main(String[] args) {
+        System.out.println("Aqui você pode introduzir um número e receber o dia correspondente da semana");
+
+    }
+}
