2ea6b5059dbbd76310db011e474f97af97a64c7f
p3rsidh <persidh.pimenta@gmail.com>
2022-06-28 11:12:09 -0300

    Criando comparações para identificar o menor valor, imprimindo o mesmo para o usuário.

--- src/lista03A/exercicio02/ProdutoRentavel.java
+++ src/lista03A/exercicio02/ProdutoRentavel.java
@@ -4,17 +4,25 @@ import java.util.Scanner;
 
 public class ProdutoRentavel {
     public static void main(String[] args) {
-        float produto1, produto2, produto3, maisBarato;
+        float produto1, produto2, produto3;
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Aqui voce pode informar o preço de 3 produtos, e receberá qual é o mais barato");
+        System.out.println("Aqui voce pode informar o preï¿½o de 3 produtos, e receberï¿½ qual ï¿½ o mais barato");
         System.out.println("Informe o valor do primeiro produto");
         produto1 = input.nextFloat();
         System.out.println("Informe o valor do segundo produto");
         produto2 = input.nextFloat();
-        System.out.println("Informe o terceiro número");
+        System.out.println("Informe o valor do terceiro produto");
         produto3 = input.nextFloat();
 
-
+        if (produto1 < produto2 && produto1 < produto3) {
+            System.out.println("O primeiro produto Ã© o mais barato, custando " + produto1);
+        } else if (produto2 < produto1 && produto2 < produto3) {
+            System.out.println("O segundo produto Ã© o mais barato custando " + produto2);
+        } else if (produto3 < produto1 && produto3 < produto2) {
+            System.out.println("O terceiro produto Ã© o mais barato, custando " + produto3);
+        } else if (produto1 == produto2 && produto2 == produto3) {                            //Se o 1 Ã© igual ao 2, e o 2 Ã© igual ao 3, automaticamente o 1 Ã© igual ao 3.
+            System.out.println("Os produtos tem o mesmo valor, custando " + produto1);
+        }
     }
 }
