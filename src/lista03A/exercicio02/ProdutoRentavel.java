84e763b92c2f664e8d12f6fc67ac6bb7765a10d4
p3rsidh <persidh.pimenta@gmail.com>
2022-06-28 10:59:14 -0300

    Criando pasta que contem o algorítimo, este recebe 3 valores de produto e identifica o mais barato.

---
+++ src/lista03A/exercicio02/ProdutoRentavel.java
@@ -0,0 +1,20 @@
+package lista03A.exercicio02;
+
+import java.util.Scanner;
+
+public class ProdutoRentavel {
+    public static void main(String[] args) {
+        float produto1, produto2, produto3, maisBarato;
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Aqui voce pode informar o preço de 3 produtos, e receberá qual é o mais barato");
+        System.out.println("Informe o valor do primeiro produto");
+        produto1 = input.nextFloat();
+        System.out.println("Informe o valor do segundo produto");
+        produto2 = input.nextFloat();
+        System.out.println("Informe o terceiro número");
+        produto3 = input.nextFloat();
+
+
+    }
+}
