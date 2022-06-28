cf1bb1b17e661313c37de8cb332c62e6fbf5ec36
p3rsidh <persidh.pimenta@gmail.com>
2022-06-28 11:35:41 -0300

    Criando condições de valores e cálculos, corrigindo erros

--- src/lista03A/exercicio03/SeAprovado.java
+++ src/lista03A/exercicio03/SeAprovado.java
@@ -11,10 +11,25 @@ public class SeAprovado {
         System.out.println("Este programa receberá duas notas de um aluno, calculará a media e informará se o aluno foi aprovado");
         System.out.println("Introduza o nome do aluno");
         nome = input.next();
-        System.out.println("Introduza a primeira nota");
+        System.out.println("Introduza a primeira nota,  entre 0 e 10");
         nota1 = input.nextFloat();
-        System.out.println("Introduza a segunda nota");
+        System.out.println("Introduza a segunda nota, entre 0 e 10");
         nota2 = input.nextFloat();
         System.out.println("Processando");
+
+
+        if(nota1 > 10 || nota2 > 10 || nota1 < 0 || nota2 < 0){
+            System.out.println("Introduza valores válidos, entre 0 e 10");
+        }else {
+            media = (nota1 + nota2)/2;
+        if (media >= 7){
+            if (media == 10){
+                System.out.println("Aluno " +nome +" aprovado com Distinção!! Com média 10");
+            }else {
+                System.out.println("Aluno " +nome + " aprovado com média de " + media);
+            }
+        }else if( media < 7){
+            System.out.println("Aluno " +nome +" reprovado com media de " +media);
+        }}
     }
 }
