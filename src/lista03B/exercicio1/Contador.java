f61f7ae37477910318daa387290370c08a38c00c
p3rsidh <persidh.pimenta@gmail.com>
2022-06-28 13:00:09 -0300

    Criando nova classe que conte de 0 a 100

---
+++ src/lista03B/exercicio1/Contador.java
@@ -0,0 +1,10 @@
+package lista03B.exercicio1;
+
+public class Contador {
+    public static void main(String[] args) {
+        System.out.println("Contaremos de 1 a 100");
+        for (int i = 1; i<101; i++){
+            System.out.println(i);
+        }
+    }
+}
