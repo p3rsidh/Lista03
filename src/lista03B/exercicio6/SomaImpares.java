fd9de7a09360cc824ac3ba6ab4e094d1e9733c64
p3rsidh <persidh.pimenta@gmail.com>
2022-06-28 21:28:44 -0300

    mudando "pares" para "impares" na linha 12. Retirando o print do for e levando para fora

--- src/lista03B/exercicio6/SomaImpares.java
+++ src/lista03B/exercicio6/SomaImpares.java
@@ -9,7 +9,7 @@ public class SomaImpares {
         int inicio, fim, soma = 0;
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Mostra soma de numeros pares no seu intervalo");
+        System.out.println("Mostra soma de numeros impares no seu intervalo");
         System.out.println("Escreva o inicio do intervalo");
         inicio = input.nextInt();
         System.out.println("Escreva o final do intervalo");
@@ -19,11 +19,10 @@ public class SomaImpares {
                 if(i % 2 != 0 ){
                     soma = soma + i;
                 }
-                System.out.println("A soma dos valores é: " +soma);
             }
         }else {
             System.out.println("Intervelo de valores invalido");
         }
-
+        System.out.println("A soma dos valores é: " +soma);
     }
 }
