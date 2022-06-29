50ffbf68a402c3eaf04a50e30e241ccbc2a2b781
p3rsidh <persidh.pimenta@gmail.com>
2022-06-28 21:58:30 -0300

    Adicionando mudança de boolean, acrescentando resultado

--- src/lista03B/exercicio8/SomaImpares.java
+++ src/lista03B/exercicio8/SomaImpares.java
@@ -2,15 +2,19 @@ package lista03B.exercicio8;
 
 public class SomaImpares {
     public static void main(String[] args) {
-        int soma = 0, i = 0;
+        int soma = 0, i = 0, quantidade=0;
         boolean numeros = false;
 
         while (numeros == false){
             i++;
             if (i%2 != 0){
                 soma = soma + i;
+                quantidade++;
+                if (quantidade == 50){
+                    numeros = true;
+                }
             }
-            System.out.println(soma);
         }
+        System.out.println("A soma dos numeros e: " +soma);
     }
 }
