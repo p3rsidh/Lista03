67243719e3b2d237d1ea3632ba8cecb3fa2dd03f
p3rsidh <persidh.pimenta@gmail.com>
2022-06-28 21:10:58 -0300

    Criando classe e algoritimo que soma de 1000 em 1000, até 100000

---
+++ src/lista03B/exercicio5/InteiroSomando.java
@@ -0,0 +1,12 @@
+package lista03B.exercicio5;
+
+public class InteiroSomando {
+    public static void main(String[] args) {
+        int Somando = 0;
+
+        while (Somando != 100000){
+            Somando = Somando + 1000;
+            System.out.println(Somando);
+        }
+    }
+}
